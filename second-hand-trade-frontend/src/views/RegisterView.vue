1e96b1eb6e528f84fca4ce24cd47805747a524e8
王凯涛 <wangkt@archempower.com>
2026-04-13 15:30:58 +0800

    手机号设置为必填

--- second-hand-trade-frontend/src/views/RegisterView.vue
+++ second-hand-trade-frontend/src/views/RegisterView.vue
@@ -109,6 +109,11 @@ const rules = reactive<FormRules>({
   ],
   phone: [
     {
+      required: true,
+      message: "请输入手机号",
+      trigger: "blur",
+    },
+    {
       pattern: /^1[3-9]\d{9}$/,
       message: "请输入正确的手机号",
       trigger: "blur",
